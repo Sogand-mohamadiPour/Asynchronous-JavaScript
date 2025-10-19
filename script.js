f1c1a17b657f045135eb6a028673eb14990a1a89
Sogand <sogandmohamadipour7@gmail.com>
2025-10-19 22:22:47 +0330

    v2 with neighbour is added

--- script.js
+++ script.js
@@ -98,19 +98,3 @@ const getCountryAndNeighbour = function (country) {
 // Test with countries
 getCountryAndNeighbour('usa');
 
-// CallBack hell structure
-setTimeout(() => {
-    console.log('1 second passed');
-    setTimeout(() => {
-        console.log('2 seconds passed');
-        setTimeout(() => {
-            console.log('3 seconds passed');
-        }, 1000);
-        setTimeout(() => {
-            console.log('4 seconds passed');
-        }, 1000);
-        setTimeout(() => {
-            console.log('5 seconds passed');
-        }, 1000);
-    }, 1000);
-}, 1000);
