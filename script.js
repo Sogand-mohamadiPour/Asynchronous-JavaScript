a3a26ed2c6436417df77bd3ebc40c0529c7d0279
Sogand <sogandmohamadipour7@gmail.com>
2025-10-21 22:13:12 +0330

    handling rejected promises

--- script.js
+++ script.js
@@ -3,6 +3,34 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+// Function to render a country card
+const renderCountry = function (data, className = '') {
+  // Take neccesary information from obj
+  const language = Object.values(data.languages)[0];
+  const currency = Object.values(data.currencies)[0];
+
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.continents}</h4>
+        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
+        <p class="country__row"><span>🗣️</span>${language}</p>
+        <p class="country__row"><span>💰</span>${currency.name} (${currency.symbol})</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 
 // const getCountryData = function(country){
@@ -41,28 +69,7 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 //v2
-// Function to render a country card
-const renderCountry = function (data, className = '') {
-  // Take neccesary information from obj
-  const language = Object.values(data.languages)[0];
-  const currency = Object.values(data.currencies)[0];
 
-  const html = `
-    <article class="country ${className}">
-      <img class="country__img" src="${data.flags.png}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name.common}</h3>
-        <h4 class="country__region">${data.continents}</h4>
-        <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}M people</p>
-        <p class="country__row"><span>🗣️</span>${language}</p>
-        <p class="country__row"><span>💰</span>${currency.name} (${currency.symbol})</p>
-      </div>
-    </article>
-  `;
-
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
 
 // Function to get country data from API
 // const getCountryAndNeighbour = function (country) {
@@ -138,7 +145,10 @@ const getCountryData = function (country) {
     })
     .then(response => response.json())
     .then(data => renderCountry(data[0], 'neighbour'))
-    .catch(err => alert(err))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    });
 };
 
 btn.addEventListener('click', function () {
